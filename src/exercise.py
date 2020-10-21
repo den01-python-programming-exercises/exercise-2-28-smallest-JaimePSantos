c3ab38b966f7a58fd08688592bf578f5711ca38a
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 19:45:05 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
+def smallest(number1, number2):
+    # write your code here
+    # do not print anything inside the method
+    # there must be a return command at the end
+    if(number1>number2):
+      return number2
+    else:
+      return number1
+ 
 def main():
-    #write your code below this line
-
+    answer =  smallest(2, 7)
+    print("Smallest: " + str(answer))
 if __name__ == '__main__':
     main()
